2177d85a41ecca744954d36ed27645c4d26a5e90
Bilal <bilal.ayachi@hetmml.nl>
2023-10-14 19:32:32 +0200

    fixed cleaning bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-let tree = new BST();
+let tree = new BST(); /* Check the bst.js file for the implemenation of the BST */
 
 let state = document.getElementById("option");
+
+let staer = 0;
+
+function cleanLogs () {
+    debugger;
+    while (document.getElementById("outputLog").firstChild) {
+        document.getElementById("outputLog").removeChild(document.getElementById("outputLog").firstChild)
+    }
+};
 
 function redow () {
     document.getElementById("inputNumber").style.display = "inline";
@@ -8,17 +17,23 @@ function redow () {
 
     document.getElementById("ok").style.marginTop = "0px"
     document.getElementById("ok").style.width = "6%"
-    document.getElementById("output").innerText = "";
+    // document.getElementById("output").innerText = "";
 }
 
 document.getElementById('option').addEventListener('click', (e) => {
     if (state.value == "inOrder") {
+        document.getElementById("output").innerText = "";
+        cleanLogs();
         console.log("Setting inOrder");
         document.getElementById("inputNumber").style.display = "none";
         document.getElementById("ok").style.marginTop = "10px"
         document.getElementById("ok").style.width = "100%"
+    } else if (state.value == "hasKey") {
+        cleanLogs();
+        redow();
     } else {
         redow();
+        document.getElementById("output").innerText = "";
     }
 })
 
@@ -30,7 +45,6 @@ document.getElementById("ok").addEventListener('click', () => {
         let outpuLogText = document.createTextNode("Inserting: " +  document.getElementById("inputNumber").value + " to the BST" )
         outputLog.appendChild(outpuLogText);
         document.getElementById("outputLog").appendChild(outputLog);
-
     } else if (state.value == "hasKey") {
         document.getElementById("output").innerText = tree.hasKey((document.getElementById("inputNumber").value))
     } else if (state.value == "inOrder") {
@@ -38,3 +52,4 @@ document.getElementById("ok").addEventListener('click', () => {
         document.getElementById("output").innerText = order;
     }
 });
+
